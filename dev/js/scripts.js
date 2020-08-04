c5f2205b078a03a6e79c5e12c06c14cbc950bcee
BrandonSandoval46 <brandonsandoval194@gmail.com>
2020-08-03 23:40:17 -0500

    Funcionamiento Y Estilos

---
+++ dev/js/scripts.js
@@ -0,0 +1,10 @@
+const needleHour = document.getElementById('needleHour')
+const needleMinute = document.getElementById('needleMinute')
+const needleSecond = document.getElementById('needleSecond')
+
+setInterval(() => {
+    const date = new Date()
+    needleSecond.style.transform = `rotate(${date.getSeconds() * 6}deg)`
+    needleMinute.style.transform = `rotate(${date.getMinutes() * 6}deg)`
+    needleHour.style.transform = `rotate(${((date.getHours() > 12? date.getHours() - 12  : date.getHours())*30)+(date.getMinutes() * .5)}deg)`
+}, 1000)
