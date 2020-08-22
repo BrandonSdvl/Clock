cd0577445a4dbbc31713985122d7957467187300
BrandonSandoval46 <brandonsandoval194@gmail.com>
2020-08-21 22:36:05 -0500

    Simplifying Code

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,16 +1,12 @@
-const needleHour = document.getElementById('needleHour')
-const needleMinute = document.getElementById('needleMinute')
-const needleSecond = document.getElementById('needleSecond')
+const needleHour = document.getElementById('needleHour');
+const needleMinute = document.getElementById('needleMinute');
+const needleSecond = document.getElementById('needleSecond');
 
 const setTime = () => {
     const date = new Date()
     needleSecond.style.transform = `rotate(${date.getSeconds() * 6}deg)`
     needleMinute.style.transform = `rotate(${date.getMinutes() * 6}deg)`
-    needleHour.style.transform = `rotate(${((date.getHours() > 12? date.getHours() - 12  : date.getHours())*30)+(date.getMinutes() * .5)}deg)`
+    needleHour.style.transform = `rotate(${((date.getHours() > 12 ? date.getHours() - 12 : date.getHours()) * 30) + (date.getMinutes() * .5)}deg)`
+    return setTimeout(setTime, 1000)
 }
-
-setTime()
-
-setInterval(() => {
-    setTime()
-}, 1000)
+setTime();
